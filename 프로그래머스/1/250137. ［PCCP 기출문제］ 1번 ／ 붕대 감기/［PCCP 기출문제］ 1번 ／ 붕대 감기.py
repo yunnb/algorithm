fe533e8fa8a6df0abbd90930b638a3c1af9f0163
yunnb <102670510+yunnb@users.noreply.github.com>
2025-05-23 16:12:09 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.38 ms, Memory: 9.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.py
@@ -0,0 +1,18 @@
+def solution(b, h, a):
+    c_h = h
+    cnt = 0 
+    for i in range(a[len(a)-1][0] +1):
+        if i == a[0][0]:  
+            if c_h - a[0][1] > 0:
+                c_h -= a[0][1]
+                cnt = 0
+                a.pop(0)
+            else:
+                return -1
+        else:       
+            cnt += 1
+            c_h = c_h + b[1] if c_h + b[1] < h else h
+            if cnt == b[0]: 
+                c_h = min(c_h+b[2], h)
+                cnt = 0
+    return c_h

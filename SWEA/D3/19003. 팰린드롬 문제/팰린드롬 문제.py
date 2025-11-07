eb088eb99f2e967b1ac5989d84f5de6ddb453cdc
yunnb <102670510+yunnb@users.noreply.github.com>
2025-11-07 12:22:14 +0900

    [D3] Title: 팰린드롬 문제, Time: 152 ms, Memory: 62,464 KB -BaekjoonHub

---
+++ SWEA/D3/19003. 팰린드롬 문제/팰린드롬 문제.py
@@ -0,0 +1,27 @@
+t = int(input())
+
+for t in range(1, t + 1):
+    n, m = map(int, input().split())
+    strings = [list(input()) for _ in range(n)]
+
+    ans = 0
+    center = ''
+    while strings:
+        string = strings.pop()
+        rev = string[::-1]
+
+        if string == rev:
+            center += ''.join(string)
+            if center == center[::-1]:
+                ans += len(rev)
+                continue
+
+        if rev in strings:
+            rev_idx = strings.index(rev)
+            strings.pop(rev_idx)
+            ans += len(rev) * 2
+
+    print(f'#{t}', ans)
+
+# 1. abc cba -> 이렇게 뒤집어서 같은 문자열이 있다면 둘다 채택 (전체 문자열의 양옆에 배치)
+# 2. ded -> 이런식으로 하나만을 뒤집었을 때 같다면 채택 (전체 문자열의 가운데에 배치)

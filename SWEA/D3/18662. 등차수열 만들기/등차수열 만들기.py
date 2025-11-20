399cb56597da0693f2f4b6b341816c11f009e31a
yunnb <102670510+yunnb@users.noreply.github.com>
2025-11-20 20:46:49 +0900

    [D3] Title: 등차수열 만들기, Time: 66 ms, Memory: 51,200 KB -BaekjoonHub

--- SWEA/D3/18662. 등차수열 만들기/등차수열 만들기.py
+++ SWEA/D3/18662. 등차수열 만들기/등차수열 만들기.py
@@ -1,7 +1,8 @@
 t = int(input())
 
-for t in range(1, t + 1):
+for tc in range(1, t + 1):
     a, b, c = map(int, input().split())
 
-    ans = abs(((2 * b) - a - c) / 2.0)
-    print(f'#{t} {ans:.1f}')
+    ans = abs(min(float(2 * b + a + c), (-2 * b + a + c) / 2))
+
+    print('#{0:} {1:.1f}'.format(tc, ans))

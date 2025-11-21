2fa34c00898fd5f64140ced51bc58916890e23fc
yunnb <102670510+yunnb@users.noreply.github.com>
2025-11-21 20:46:24 +0900

    [D2] Title: 달팽이 숫자, Time: 63 ms, Memory: 52,608 KB -BaekjoonHub

---
+++ SWEA/D2/1954. 달팽이 숫자/README.md
@@ -0,0 +1,15 @@
+# [D2] 달팽이 숫자 - 1954 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5PobmqAPoDFAUq) 
+
+### 성능 요약
+
+메모리: 52,608 KB, 시간: 63 ms, 코드길이: 623 Bytes
+
+### 제출 일자
+
+2025-11-21 20:46
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.py
@@ -0,0 +1,33 @@
+t = int(input())
+
+dxs, dys = [0, 1, 0, -1], [1, 0, -1, 0]
+
+
+def in_range(x, y):
+    return 0 <= x < n and 0 <= y < n
+
+
+for tc in range(1, t + 1):
+    n = int(input())
+    grid = [[0] * n for _ in range(n)]
+
+    x, y = 0, 0
+    cnt = 1
+    d = 0
+
+    for _ in range(n * n):
+        grid[x][y] = cnt
+
+        nx, ny = x + dxs[d], y + dys[d]
+        if in_range(nx, ny) and grid[nx][ny] == 0:
+            grid[nx][ny] = cnt
+            x, y = x + dxs[d], y + dys[d]
+        else:
+            d = (d + 1) % 4
+            x, y = x + dxs[d], y + dys[d]
+
+        cnt += 1
+
+    print(f'#{tc}')
+    for g in grid:
+        print(*g)
